8c80dce37a02fa39720c0c46cdb603871c0d40ec
davidalves1 <alves.david@outlook.com>
2016-07-11 20:55:37 -0300

    Atualização das mensagens

--- bin/index.js
+++ bin/index.js
@@ -31,26 +31,28 @@ https
 			    	var temperatura = (res.temperature - 32) / 1.8;
 			    	var sensacao = (res.apparentTemperature - 32) / 1.8;
 			    	var icone = res.icon.replace(/\W/g, '');
-			    	console.log(`${busca.toUpperCase()}: ${eval('mensagem.' + icone)}.` +
+			    	console.log(`${busca.toUpperCase()}: ${eval('mensagem.' + icone)}` +
 			    	 	` A temperatura no momento é aproximadamente ${temperatura.toFixed(1)}°c` +
 			    	 	` com sensação térmica de ${sensacao.toFixed(0)}°c`);
 			    })
 			    .catch((err) => {
-			        console.log('Error!!!', err);
+			        console.log('Ops, algo de errado aconteceu. :( \n\n' + 
+			        	`Error:  ${err.split('\n')[1]}` +
+			        	'\n\nPor favor, informe o erro acima em: https://github.com/davidalves1/clima-app/issues');
 				});
 		});
 	});
 	
 
 var mensagem = {
-	clearday: 'Dia com céu claro', 
-	clearnight: 'Noite com céu claro',	 
-	rain: 'No momento chove', 
-	snow: 'No momento neva', 
-	sleet: 'No momento neva e chove', 
-	wind: 'No momento venta forte', 
-	fog: 'No momento a neblima cobre a cidade', 
-	cloudy: 'Tempo encoberto', 
+	clearday: 'Dia com céu claro.', 
+	clearnight: 'Noite com céu estrelado.',	 
+	rain: 'Prepare o guarda chuva, pois parece que vai chover.', 
+	snow: 'Tire as cobertas do guarda-roupas, pois parece que o frio vem com tudo.', 
+	sleet: 'Prepare seu chá, pois parece que vem chuva e frio.', 
+	wind: 'Macacos me mordam! Que vento é esse hein?!', 
+	fog: 'Hoje a neblina parece ter vindo com tudo.', 
+	cloudy: 'Tempo encoberto. :|', 
 	partlycloudyday: 'Dia parcialmente nublado',
 	partlycloudynight: 'Noite parcialmente nublada'
 };
