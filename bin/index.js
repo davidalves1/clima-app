15bebd7d3c78227b059eedeb0e39210db997778d
David <dev.david@aticonsultoria.com>
2016-07-13 10:20:05 -0300

    Remover sensação térmica

--- bin/index.js
+++ bin/index.js
@@ -5,7 +5,7 @@
 const https = require('https');
 const ForecastIO = require('forecast-io');
 // Faça o cadastro em https://developer.forecast.io/ para obtera a chave da api e substitua abaixo.
-const forecast = new ForecastIO('your-forecast-api-key');
+const forecast = new ForecastIO('forecast-api-key');
 const querystring = require('querystring');
 const meow = require('meow');
 
@@ -31,7 +31,7 @@ https
 		response.on('error', (e) => {
 			console.log('Ops, cidade não encontrada! :(');
 		});
-		response.on('end', function(){
+		response.on('end', () => {
 			 let location = JSON.parse(body).results[0].geometry.location;
       		
       		forecast
@@ -41,11 +41,9 @@ https
 				.then((res) => {
 					res = JSON.parse(res).currently;            
 			    	let temperatura = (res.temperature - 32) / 1.8;
-			    	let sensacao = (res.apparentTemperature - 32) / 1.8;
 			    	let icone = res.icon.replace(/\W/g, '');
 			    	console.log(`${busca.toUpperCase()}: ${eval('mensagem.' + icone)}` +
-			    	 	` A temperatura no momento é aproximadamente ${Math.round(temperatura)}°c` +
-			    	 	` com sensação térmica de ${Math.round(sensacao)}°c`);
+			    	 	` A temperatura no momento é aproximadamente ${Math.round(temperatura)}°c.`);
 			    })
 			    .catch((err) => {
 			        console.log('Ops, algo de errado aconteceu. :( \n\n' + 
@@ -66,6 +64,6 @@ const mensagem = {
 	wind: 'Macacos me mordam! Que vento é esse hein?!', 
 	fog: 'Hoje a neblina parece ter vindo com tudo.', 
 	cloudy: 'Tempo encoberto.', 
-	partlycloudyday: 'Dia parcialmente nublado',
-	partlycloudynight: 'Noite parcialmente nublada'
+	partlycloudyday: 'Dia parcialmente nublado.',
+	partlycloudynight: 'Noite parcialmente nublada.'
 };
